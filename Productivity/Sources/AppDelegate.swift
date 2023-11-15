adc95745e4e6fb82802eb49109d152d8729644f1
Javier Fernandes <15007176+javierjosefernandes@users.noreply.github.com>
2023-11-15 13:48:38 +0100

    Update AppDelegate.swift

--- Productivity/Sources/AppDelegate.swift
+++ Productivity/Sources/AppDelegate.swift
@@ -5,16 +5,6 @@
 //  Created by Javier Fernandes on 14/11/2023.
 //
 
-import Amplify
-import AWSAPIPlugin
-import AWSCognitoAuthPlugin
-import AWSCognitoIdentity
-import AWSCognitoIdentityProvider
-import AWSDataStorePlugin
-import AWSPluginsCore
-import Foundation
-import Mixpanel
-import Sentry
 import UIKit
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
