688a88865b0d52652462cb4cf136b96e7ddaffcd
Javier Fernandes <15007176+javierjosefernandes@users.noreply.github.com>
2023-11-15 10:29:56 +0100

    Update ProductivityTests.swift

--- ProductivityTests/ProductivityTests.swift
+++ ProductivityTests/ProductivityTests.swift
@@ -18,14 +18,4 @@ final class ProductivityTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
