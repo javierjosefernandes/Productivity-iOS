5dd6156f475dfb88e0faa7f9525d08c70b444949
Javier Fernandes <15007176+javierjosefernandes@users.noreply.github.com>
2023-11-16 16:33:38 +0100

    Remove `Codecov` from `test` workflow and restore dependencies

--- Productivity/Sources/AppDelegate.swift
+++ Productivity/Sources/AppDelegate.swift
@@ -5,7 +5,16 @@
 //  Created by Javier Fernandes on 14/11/2023.
 //
 
+import Amplify
+import AWSAPIPlugin
+import AWSCognitoAuthPlugin
+import AWSCognitoIdentity
+import AWSCognitoIdentityProvider
+import AWSDataStorePlugin
+import AWSPluginsCore
 import Foundation
+import Mixpanel
+import Sentry
 import UIKit
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
@@ -14,7 +23,22 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        true
+        if let mixpanelToken = AppConfig.shared.stringValue(forKey: "MixpanelProjectToken") {
+            Mixpanel.initialize(token: mixpanelToken, trackAutomaticEvents: true)
+        }
+
+        let sentryDNS = AppConfig.shared.stringValue(forKey: "SentryDNS")
+        let sentryEnv = AppConfig.shared.stringValue(forKey: "SentryEnvironment")
+        if let sentryDNS, let sentryEnv {
+            SentrySDK.start { options in
+                options.dsn = sentryDNS
+                options.environment = sentryEnv
+                options.debug = sentryEnv == "development"
+                options.enableTracing = sentryEnv == "production"
+            }
+        }
+
+        return true
     }
 
 }
